db846ee7a2d3275f0a9a64f8b6ba8719715f5b3c
Elshirbini <ahmedalshirbini33@gmail.com>
2025-07-03 14:07:11 +0300

    modified:   docker-compose.yml 	deleted:    package-lock.json 	deleted:    package.json 	modified:   server/config/db.js 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.NODE_ENV === "development" ? "localhost" : "postgres",
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false,
